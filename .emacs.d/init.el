551629d5f58de6cc70b901a9d474961f931cc8b3
Jorge Rubio <284835557+jorgerubiocr@users.noreply.github.com>
2026-06-05 05:47:07 -0600

    update emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,10 +4,6 @@
 (tool-bar-mode -1)
 (load-theme 'manoj-dark t)
 
-;; Set font
- (when (member "Iosevka" (font-family-list))
-   (set-face-attribute 'default nil :font "Iosevka" :height 150))
-
 ;; Set column of numbers
 (global-display-line-numbers-mode t)
 
@@ -20,6 +16,9 @@
 (setq scroll-conservatively 10000)
 (setq scroll-margin 10)
 (setq scroll-preserve-screen-position t)
+
+(when (member "Iosevka Nerd Font" (font-family-list))
+  (set-face-attribute 'default nil :font "Iosevka Nerd Font" :height 150))
 
 ;; Put autosave files (ie #foo#) and backup files (ie foo~) in ~/.emacs.d/.
 (custom-set-variables
@@ -34,13 +33,18 @@
 (make-directory "~/.emacs.d/autosaves/" t)
 (make-directory "~/.emacs.d/backups/" t)
 
-;; Packages
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(setq package-enable-at-startup nil)
-(package-initialize)
-
-(unless (package-installed-p 'dart-mode)
-  (unless package-archive-contents
-    (package-refresh-contents))
-  (package-install 'dart-mode))
+(use-package dart-mode
+  :ensure t
+  :mode "//.dart//;")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
